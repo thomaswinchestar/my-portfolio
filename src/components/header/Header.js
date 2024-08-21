a2ac17a89b08cc15c6fec8e8b9981a9c1f755c3e
Devpph <52081212+thomaswinchestar@users.noreply.github.com>
2024-08-22 03:21:01 +0700

    added valid navigate address

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -73,7 +73,7 @@ function Header() {
             <a href="#contact">Contact Me</a>
           </li>
           <li>
-            <a>
+            <a href="#toggle-switch">
               <ToggleSwitch />
             </a>
           </li>
